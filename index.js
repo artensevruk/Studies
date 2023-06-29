ef1ab604a7f2713a76abbe8033167dff06d87f0b
Artem Sevruk <96494811+artensevruk@users.noreply.github.com>
2023-06-29 12:13:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const batton = document.getElementById("#batton");
+batton.addEventListener("click",batton);
+function batton(){
+    const text = document.querySelector("Card_Text");
+    text.style.color ='
+    color:red;
+    ';
+}
